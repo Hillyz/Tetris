5889c934d2088fc57ff069f9aa51d487f05a7039
Hillyz <amund2003@gmail.com>
2020-11-20 14:54:25 +0100

    Some collision

--- pygameGlobals.py
+++ pygameGlobals.py
@@ -1,3 +1,7 @@
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 import pygame as pg
 
 pg.init()
--- tetris.py
+++ tetris.py
@@ -35,7 +35,11 @@ class Block(pg.sprite.Sprite):
 
 
     def collision(self, group2):
+<<<<<<< Updated upstream
         collision = pg.sprite.spritecollide(self, group2, True)
+=======
+        collision = pg.sprite.spritecollide(self, group2, False, )
+>>>>>>> Stashed changes
         if collision:
             return True
 
@@ -48,6 +52,7 @@ class Tetrimino(Block):
 
 
 #draws a grid to show possible block movement
+<<<<<<< Updated upstream
 #TAKES TOO MUCH TIME TO DRAW EACH FRAME, FIND SOLOUTION
 def drawGrid():
     cellsize = win_x//10
@@ -60,6 +65,13 @@ def drawGrid():
     return gridlst
 
 
+=======
+def drawGrid():
+    for x in range(0,win_x, win_x//10):
+        pg.draw.line(SCREEN, (200, 200, 200), (x, 0), (x, win_y))
+    for y in range(0, win_y, win_y//20):
+        pg.draw.line(SCREEN, (200, 200, 200), (0, y), (win_x, y))
+>>>>>>> Stashed changes
 
 
 def main():
@@ -68,10 +80,17 @@ def main():
     block = Block()
     fpsCounter = 0
     movement_delay = 0
+<<<<<<< Updated upstream
     live_blocks = pg.sprite.Group()
     dead_blocks = pg.sprite.Group()
     live_blocks.add(block)
     gridlst = drawGrid()
+=======
+    insta_delay = 0
+    live_blocks = pg.sprite.Group()
+    dead_blocks = pg.sprite.Group()
+    live_blocks.add(block)
+>>>>>>> Stashed changes
 
 #Game loop
     while run:
@@ -90,6 +109,14 @@ def main():
         #Block movement
         for block in live_blocks:
             movement_delay += 1
+<<<<<<< Updated upstream
+=======
+            insta_delay += 1
+            #Makes sure that there is no bug because of collision
+            if len(live_blocks) > 1:
+                block.remove(live_blocks)
+
+>>>>>>> Stashed changes
             #Player key input check
             if keys[pg.K_LEFT] and movement_delay % 5 == 0:
                 block.move_left()
@@ -104,13 +131,21 @@ def main():
 
             #Block is placed at the bottom, new block is spawned
             #Forced by player
+<<<<<<< Updated upstream
             if keys[pg.K_SPACE] and movement_delay % 10 == 0:
+=======
+            if keys[pg.K_SPACE] and insta_delay > 0:
+>>>>>>> Stashed changes
                 block.rect.y = win_y - win_y//20
                 dead_blocks.add(block)
                 block.remove(live_blocks)
                 newblock = Block()
                 live_blocks.add(newblock)
+<<<<<<< Updated upstream
                 movement_delay = 0
+=======
+                insta_delay = -50
+>>>>>>> Stashed changes
 
             #naturally by block movement
             elif block.rect.y >= win_y - win_y//20:
@@ -120,6 +155,7 @@ def main():
                 live_blocks.add(newblock)
 
 
+<<<<<<< Updated upstream
             ## ??????
             #for block in dead_blocks:
             #    block.rect.topleft = (block.rect.x, win_y - win_y//20)
@@ -127,10 +163,22 @@ def main():
             # Checks if there is collision
             if block.collision(dead_blocks):
                 print("test")
+=======
+            # Checks if there is collision
+            if block.collision(dead_blocks):
+                print("Collision", block.rect.y)
+>>>>>>> Stashed changes
                 dead_blocks.add(block)
                 block.remove(live_blocks)
                 newblock = Block()
                 live_blocks.add(newblock)
+<<<<<<< Updated upstream
+=======
+                ## ??????
+                block.rect.topleft = (block.rect.x, block.rect.y - win_y//20)
+                #for block in dead_blocks:
+                #    block.rect.topleft = (block.rect.x, block.rect.y - win_y//20)
+>>>>>>> Stashed changes
 
         #Update
         live_blocks.update()
@@ -140,6 +188,10 @@ def main():
         SCREEN.fill(BLACK)
         live_blocks.draw(SCREEN)
         dead_blocks.draw(SCREEN)
+<<<<<<< Updated upstream
+=======
+        drawGrid()
+>>>>>>> Stashed changes
 
         #Flip display
         pg.display.update()
